c41c92ca64efa97e32d5187cfb3ab93b7eb2ce7b
Peter Mösenthin <moesenthin@sipgate.de>
2019-07-09 16:57:41 +0200

    feat: add tts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as client from "ari-client";
+import * as base64 from "base-64";
+import * as fsPromise from "fs-extra";
 import { StasisStart } from "./stasis";
+import { synthesize } from "./tts";
 
 export const CONFIG: any = {
 	asterisk: {
@@ -14,8 +17,34 @@ export const CONFIG: any = {
 	}
 };
 
+async function loadGcpCredentials(): Promise<void> {
+	return new Promise(async (resolve, reject) => {
+		console.log("Loading GCP credentials");
+		const gcpCrendtialsBase64: string = process.env.GCP_CREDENTIALS;
+		const gcpCrendtialsFile: string = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+		if (gcpCrendtialsFile) {
+			console.log("Using provided GCP credentials file", gcpCrendtialsFile);
+			resolve();
+			return;
+		}
+		if (!gcpCrendtialsBase64) {
+			// credentials are provided by env
+			reject("No GCP credentials provided!");
+			return;
+		}
+		console.log("GCP credentials provided as base64, writing to ./gcp.json");
+		process.env.GOOGLE_APPLICATION_CREDENTIALS = "./gcp.json";
+		const gcpDecoded: any = base64.decode(gcpCrendtialsBase64);
+		await fsPromise.writeFile("gcp.json", gcpDecoded);
+		resolve();
+	});
+}
+
 async function startApp(): Promise<void> {
 	console.log("Starting with config", CONFIG);
+
+	await loadGcpCredentials();
+
 	const {
 		asterisk: { username, port, password, host, application }
 	} = CONFIG;
---
+++ src/tts.ts
@@ -0,0 +1,48 @@
+import { v1beta1 as textToSpeech } from "@google-cloud/text-to-speech";
+import * as fsPromise from "fs-extra";
+
+const cache: Map<string, any> = new Map();
+
+function getCacheKey(ttsRequest: any): string {
+	return JSON.stringify(ttsRequest);
+}
+
+async function writeAudioFile(name: string, content: any): Promise<void> {
+	console.log("Writing file", name);
+	await fsPromise.writeFile(name, content);
+}
+
+export async function synthesize(text: string): Promise<void> {
+	console.log("Synthesizing", { text });
+
+	try {
+		const ttsRequest: any = {
+			input: { text },
+			voice: {
+				languageCode: "de-DE",
+				name: "de-DE-Wavenet-B"
+			},
+			audioConfig: {
+				audioEncoding: "LINEAR16",
+			}
+		};
+
+		console.log("Synthesize request", ttsRequest);
+
+		const key: string = getCacheKey(ttsRequest);
+
+		if (cache.has(key)) {
+			console.log("Returning cached speech synthesis");
+			return;
+		}
+
+		const client: textToSpeech.TextToSpeechClient = new textToSpeech.TextToSpeechClient();
+
+		const [{ audioContent }] = await client.synthesizeSpeech(ttsRequest);
+
+		cache.set(key, audioContent);
+		await writeAudioFile("somefile.wav", audioContent);
+	} catch (e) {
+		console.error("Unable to synthesize text", e);
+	}
+}
